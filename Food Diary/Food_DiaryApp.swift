54ee97383073898752d17e84c6aafbb20e89903d
Alexandre Nicol <nicol.alexandre1@gmail.com>
2022-01-04 20:40:32 +0000

    Initial Commit

---
+++ Food Diary/Food_DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_DiaryApp.swift
+//  Food Diary
+//
+//  Created by Raoul on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Food_DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
